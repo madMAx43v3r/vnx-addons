fed5f8bd556095797337e89a0e6f3a8096d1c0ce
Max Wittal <wittal@neobotix.de>
2020-07-09 15:51:11 +0200

    codegen update

--- codegen.sh
+++ codegen.sh
@@ -2,6 +2,12 @@
 
 VNX_INTERFACE_DIR=${VNX_INTERFACE_DIR:-/usr/interface}
 
+VNX_INCLUDE=$1
+
+if [ -z "$VNX_INCLUDE" ]; then
+        VNX_INCLUDE=${VNX_INTERFACE_DIR}/vnx/
+fi
+
 cd $(dirname "$0")
 
-vnxcppcodegen --cleanup generated/ vnx.addons interface/ modules/ ${VNX_INTERFACE_DIR}/vnx/
+vnxcppcodegen --cleanup generated/ vnx.addons interface/ modules/ $VNX_INCLUDE
