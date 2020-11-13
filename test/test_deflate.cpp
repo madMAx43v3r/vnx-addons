6bd7d59abd74a35823d1abae3f736fb3b51487a8
Max Wittal <wittal@neobotix.de>
2020-11-13 18:52:14 +0100

    test update

--- test/test_deflate.cpp
+++ test/test_deflate.cpp
@@ -17,18 +17,26 @@ int main()
 	{
 		auto value = LogMsg::create();
 		value->level = 1337;
-		value->message.resize(1 * 1024 * 1024 + 1337);
+		value->message.resize(16 * 1024 * 1024 + 1337);
 		
+		for(auto& v : value->message) {
+			if(::rand() % 8 == 0) {
+				v = ::rand();
+			}
+		}
+
+		const auto time_begin = vnx::get_wall_time_micros();
 		auto deflated = addons::DeflatedValue::compress(value);
+		std::cout << "took " << (vnx::get_wall_time_micros() - time_begin) << " usec" << std::endl;
 		std::cout << "deflated size = " << deflated->data.size() << " bytes" << std::endl;
-		std::cout << vnx::to_string(deflated->data) << std::endl;
+//		std::cout << vnx::to_string(deflated->data) << std::endl;
 		
 		auto value2 = std::dynamic_pointer_cast<const LogMsg>(deflated->decompress());
 		if(value2->level != value->level) {
 			throw std::logic_error("value2->level != value->level");
 		}
-		if(value2->message.size() != value->message.size()) {
-			throw std::logic_error("value2->message.size() != value->message.size()");
+		if(value2->message != value->message) {
+			throw std::logic_error("value2->message != value->message");
 		}
 	}
 }
